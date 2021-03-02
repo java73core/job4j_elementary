ae5b589706efbcac59f2248e48600fc64798c69a
java73core <mrvandrey@yandex.ru>
2021-03-02 23:54:06 +0400

    6.0.1. Объявление массива. [#474923]

---
+++ src/ru/job4j/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] sh = new short[10];
+        String[] str = new String[100500];
+        float[] fl = new float[40];
+    }
+}
