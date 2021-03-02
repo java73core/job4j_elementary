a5089ae8fd5bff9fc177895abb8be996e323b638
java73core <mrvandrey@yandex.ru>
2021-03-02 23:57:13 +0400

    6.0.1.1. Размер массива [#474919]

--- src/ru/job4j/ArrayDefinition.java
+++ src/ru/job4j/ArrayDefinition.java
@@ -2,9 +2,11 @@ package ru.job4j;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-
         short[] sh = new short[10];
         String[] str = new String[100500];
         float[] fl = new float[40];
+        System.out.println(sh.length);
+        System.out.println(str.length);
+        System.out.println(fl.length);
     }
 }
